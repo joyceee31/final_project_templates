08d0456b24ea193b8ead1a3ae81496ff5e8cef52
jiaojiaojane121 <janewang121@brandeis.edu>
2023-05-14 21:42:22 -0400

    Update nyc_traffic_accident.py

--- NYC Traffic Accidents/nyc_traffic_accident.py
+++ NYC Traffic Accidents/nyc_traffic_accident.py
@@ -7,13 +7,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import silhouette_score, davies_bouldin_score,calinski_harabasz_score
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 from sklearn.cluster import KMeans
 import plotly.express as px
 import folium
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import DBSCAN
-from streamlit_folium import folium_static,st_folium
+from streamlit_folium import folium_static, st_folium
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
@@ -21,13 +21,12 @@ from sklearn.mixture import GaussianMixture
 from scipy.stats import mode
 
 
-
 st.set_page_config(
     page_title="NYC Crime Accident",
     page_icon=":red_car:",
     layout="wide",
     initial_sidebar_state="expanded",
-    
+
 )
 
 col1, col2 = st.columns([3, 1])
@@ -42,27 +41,33 @@ This powerful tool allows you to explore and analyze crime data from the vibrant
 Gain valuable insights into crime incidents, patterns, and trends across different boroughs and neighborhoods. 
  Let's dive into the data and uncover the stories behind NYC's crime landscape!
 """)
+
+
 def load_data():
     df = pd.read_csv('output_file.csv')
     df = df.rename(columns={'Latitude': 'lat', 'Longitude': 'lon'})
     return df
 
+
 def show_raw_data(df):
     st.subheader('Raw Data')
     st.write(df)
+
+
 def explore_data(df, subheader):
     st.subheader(subheader)
     print(list(df.columns))
 
     st.write(df)
 
+
 def describe_features(df):
     st.subheader('Features Description')
-    
+
     features_description = pd.DataFrame({
-        'Feature': ['Complaint ID', 'Start Date', 'Start Time', 'Offense Description', 
-                    'Completion Status', 'BORO_NM', 'Latitude', 'Longitude', 
-                    'Victim Age Group', 'Victim Sex', 'Start Hour', 'Time Bucket', 
+        'Feature': ['Complaint ID', 'Start Date', 'Start Time', 'Offense Description',
+                    'Completion Status', 'BORO_NM', 'Latitude', 'Longitude',
+                    'Victim Age Group', 'Victim Sex', 'Start Hour', 'Time Bucket',
                     'Crime_Category'],
         'Description': [
             'Unique identifier for each complaint',
@@ -83,26 +88,28 @@ def describe_features(df):
     st.write(features_description)
 
 
-
 def visualize_data(df):
     st.subheader('Visualizing Data')
 
     def plot_crimes_by_borough():
         st.write("### Distribution of Crimes by Borough")
         plt.figure(figsize=(12, 6))
-        sns.countplot(x='BORO_NM', data=df, order=df['BORO_NM'].value_counts().index)
+        sns.countplot(x='BORO_NM', data=df,
+                      order=df['BORO_NM'].value_counts().index)
         st.pyplot(plt)
 
     def plot_crimes_by_sex():
         st.write("### Distribution of Crimes by Victim Sex")
         plt.figure(figsize=(8, 6))
-        sns.countplot(x='Victim Sex', data=df, order=df['Victim Sex'].value_counts().index)
+        sns.countplot(x='Victim Sex', data=df,
+                      order=df['Victim Sex'].value_counts().index)
         st.pyplot(plt)
 
     def plot_crimes_by_age_group():
         st.write("### Distribution of Crimes by Victim Age Group")
         plt.figure(figsize=(10, 6))
-        sns.countplot(x='Victim Age Group', data=df, order=df['Victim Age Group'].value_counts().index)
+        sns.countplot(x='Victim Age Group', data=df,
+                      order=df['Victim Age Group'].value_counts().index)
         st.pyplot(plt)
 
     def plot_crimes_by_hour():
@@ -115,7 +122,8 @@ def visualize_data(df):
         st.write("### Heatmap of Crimes by Day of Week and Hour")
         df['Start Date'] = pd.to_datetime(df['Start Date'])
         df['Day of Week'] = df['Start Date'].dt.dayofweek
-        day_hour = df.groupby(['Day of Week', 'Start Hour']).size().reset_index(name='Count')
+        day_hour = df.groupby(['Day of Week', 'Start Hour']
+                              ).size().reset_index(name='Count')
         day_hour = day_hour.pivot('Day of Week', 'Start Hour', 'Count')
         plt.figure(figsize=(15, 6))
         sns.heatmap(day_hour, cmap='coolwarm')
@@ -137,7 +145,6 @@ def visualize_data(df):
 
 def show_clusters(df):
     st.header("Crime Clusters in Manhattan")
-    clustering_method = st.selectbox("Choose a clustering method", ["KMeans", "Gaussian Mixture Model", "DBScan"])
 
     df['Start Date'] = pd.to_datetime(df['Start Date'], format='%m/%d/%Y')
 
@@ -145,7 +152,8 @@ def show_clusters(df):
     df = df[df['Start Date'].dt.year == 2022]
 
     # Create a list of values to remove
-    values_to_remove = ['4. White-Collar Crimes', '5. Drug-Related Crimes', '6. Traffic Offenses', '8. Miscellaneous Crimes']
+    values_to_remove = ['4. White-Collar Crimes', '5. Drug-Related Crimes',
+                        '6. Traffic Offenses', '8. Miscellaneous Crimes']
 
     # Filter the dataset by excluding the specified values fromb the 'Crime_Category' column
     df = df[~df['Crime_Category'].isin(values_to_remove)]
@@ -153,13 +161,35 @@ def show_clusters(df):
     scope_boro = ['MANHATTAN']
     df = df[df['BORO_NM'].isin(scope_boro)]
 
+    df_crime = df[['lat', 'lon', 'Crime_Category']]
+
+    df_crime_geo = df_crime[['lat', 'lon']]
+    st.header("Davies Bouldin Score")
+    kmean = KMeans(n_clusters=20)
+    kmean = kmean.fit(df_crime_geo)
+    k_labels = kmean.predict(df_crime_geo)
+    k_score = davies_bouldin_score(df_crime_geo, k_labels)
+    gmm = GaussianMixture(n_components=13).fit(df_crime_geo)
+    g_labels = gmm.predict(df_crime_geo)
+    g_score = davies_bouldin_score(df_crime_geo, g_labels)
+
+    # Create a dataframe with the scores
+    scores_df = pd.DataFrame({
+        'KMeans': [k_score],
+        'Mixture Model': [g_score],
+        'DBScan': [0.8546630132415487]
+    }, index=['DB Score'])
+
+    # Display the table
+    st.table(scores_df)
+
+    clustering_method = st.selectbox("Choose a clustering method", [
+                                     "KMeans", "Gaussian Mixture Model", "DBScan"])
     # If DBScan is selected, provide option to choose a specific crime category
     if clustering_method == "DBScan":
-        crime_category = st.selectbox("Choose a crime category for DBScan", 
+        crime_category = st.selectbox("Choose a crime category for DBScan",
                                       ["1. Violent Crimes", "2. Sexual Crimes", "3. Property Crimes"])
         df = df[df['Crime_Category'] == crime_category]
-
-    df_crime = df[['lat', 'lon', 'Crime_Category']]
 
     if clustering_method == "KMeans":
         model = KMeans(n_clusters=20)
@@ -173,18 +203,19 @@ def show_clusters(df):
     else:
         df_crime['Cluster'] = model.fit_predict(df_crime[['lat', 'lon']])
 
-    df_cluster_centers = df_crime.groupby('Cluster').agg({'lat': 'mean', 'lon': 'mean', 'Crime_Category': lambda x: mode(x)[0][0]}).reset_index()
-    
-    m = folium.Map(location=[df_crime['lat'].mean(), df_crime['lon'].mean()], zoom_start=13)
-    
+    df_cluster_centers = df_crime.groupby('Cluster').agg(
+        {'lat': 'mean', 'lon': 'mean', 'Crime_Category': lambda x: mode(x)[0][0]}).reset_index()
+
+    m = folium.Map(location=[df_crime['lat'].mean(),
+                   df_crime['lon'].mean()], zoom_start=13)
+
     for idx, row in df_cluster_centers.iterrows():
-        folium.Marker(location=[row['lat'], row['lon']], 
-                      popup=row['Crime_Category'], 
+        folium.Marker(location=[row['lat'], row['lon']],
+                      popup=row['Crime_Category'],
                       icon=folium.Icon(color='blue')).add_to(m)
 
     # display the map in Streamlit
     folium_static(m)
-
 
 
 def draw_map(df):
@@ -195,15 +226,18 @@ def draw_map(df):
     crime_categories = df['Crime_Category'].unique().tolist()
 
     # Create the selectboxes
-    time_bucket_filter = st.selectbox('Filter map by Time Bucket', time_buckets)
-    crime_category_filter = st.selectbox('Filter map by Crime Category', crime_categories)
+    time_bucket_filter = st.selectbox(
+        'Filter map by Time Bucket', time_buckets)
+    crime_category_filter = st.selectbox(
+        'Filter map by Crime Category', crime_categories)
 
     # Filter data based on selected options
-    df = df[(df['Time Bucket'] == time_bucket_filter) & (df['Crime_Category'] == crime_category_filter)]
+    df = df[(df['Time Bucket'] == time_bucket_filter) & (
+        df['Crime_Category'] == crime_category_filter)]
 
     # Remove rows with missing lat/lon data
     df = df.dropna(subset=['lat', 'lon'])
-    
+
     # Draw the map
     st.map(df[["lat", "lon"]])
 
@@ -216,7 +250,8 @@ def preprocess_data(df):
     column_to_encode = st.selectbox("Choose a column", categorical_columns)
 
     # Perform One-Hot encoding on the selected column
-    df_encoded = pd.get_dummies(df, columns=[column_to_encode], prefix=column_to_encode)
+    df_encoded = pd.get_dummies(
+        df, columns=[column_to_encode], prefix=column_to_encode)
 
     # Show the updated DataFrame with encoded columns
     st.write(df_encoded)
@@ -225,12 +260,14 @@ def preprocess_data(df):
 
 def train_model(df):
     st.header("Training the Model")
-    feature_columns = [col for col in df.columns if col.startswith('BORO_NM_') or col.startswith('Victim Sex_') or col == 'Start Hour']
+    feature_columns = [col for col in df.columns if col.startswith(
+        'BORO_NM_') or col.startswith('Victim Sex_') or col == 'Start Hour']
     X = df[feature_columns]
     X = df[['BORO_NM_encoded', 'Victim Sex_encoded', 'Start Hour']]
     y = df['Crime_Category_encoded']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
 
     model = DecisionTreeClassifier()
     model.fit(X_train, y_train)
@@ -240,21 +277,22 @@ def train_model(df):
     return model
 
 
-
-
 def crime_trends(df):
     st.subheader('Crime Trends Over Time')
     df['Start Date'] = pd.to_datetime(df['Start Date'])
-    df_trends = df.groupby(df['Start Date'].dt.year)['Crime_Category'].count().reset_index()
-    fig = px.line(df_trends, x='Start Date', y='Crime_Category', title='Yearly Crime Trends')
+    df_trends = df.groupby(df['Start Date'].dt.year)[
+        'Crime_Category'].count().reset_index()
+    fig = px.line(df_trends, x='Start Date', y='Crime_Category',
+                  title='Yearly Crime Trends')
     st.plotly_chart(fig)
 
     st.subheader('Top {} Crimes'.format(10))
     top_n = df['Offense Description'].value_counts()[:n].reset_index()
     top_n.columns = ['Offense Description', 'Count']
-    fig = px.bar(top_n, x='Offense Description', y='Count', title='Top {} Crimes'.format(10))
+    fig = px.bar(top_n, x='Offense Description', y='Count',
+                 title='Top {} Crimes'.format(10))
     st.plotly_chart(fig)
-    
+
 
 def predict(model, df):
     st.header("Making Predictions")
@@ -264,6 +302,7 @@ def predict(model, df):
 
     df['Predicted Crime Category'] = y_pred
     st.write(df.head())
+
 
 def draw_clusters_map(df):
     st.subheader("Map of Accidents Clusters in Manhattan")
@@ -301,7 +340,8 @@ def main():
     df = load_data()
 
     # Create a selectboxed menu at sidebar
-    menu = ['Data Exploration', 'Feature Description', 'Crime Trends', 'Show Clusters', "Show Map", "Preprocessing", "Training", "Prediction"]
+    menu = ['Data Exploration', 'Feature Description', 'Crime Trends',
+            'Show Clusters', "Show Map", "Preprocessing", "Training", "Prediction"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == 'Data Exploration':
@@ -310,7 +350,7 @@ def main():
         Welcome to the data exploration section. Here you can have a look at the raw data as well as some visualizations that help understand the dataset better.
         You can select different visuals from the dropdown menu below. The options include various distributions and a heatmap of crimes.
         """)
-        explore_data(df,subheader='Raw Data')
+        explore_data(df, subheader='Raw Data')
         visualize_data(df)
     elif choice == 'Feature Description':
         describe_features(df)
@@ -343,6 +383,7 @@ def main():
         crime_trends(df)
     elif choice == 'Prediction':
         predict()
+
 
 if __name__ == '__main__':
     main()
